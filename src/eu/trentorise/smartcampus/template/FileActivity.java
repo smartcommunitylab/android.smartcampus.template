5fb26dc6d2541b9e4912ff00fad71ad0ac24a74f
raman <raman@0fd5999a-94d0-4028-93f2-09691c278cb2>
2013-03-19 09:23:12 +0000

    Bug fixes

--- src/eu/trentorise/smartcampus/template/FileActivity.java
+++ src/eu/trentorise/smartcampus/template/FileActivity.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2012-2013 Trento RISE
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ ******************************************************************************/
 package eu.trentorise.smartcampus.template;
 
 import java.io.ByteArrayOutputStream;
@@ -27,9 +42,6 @@ import eu.trentorise.smartcampus.storage.model.UserAccount;
 
 public class FileActivity extends Activity {
 
-	/**
-	 * 
-	 */
 	private static final int AUTH_REQUESTCODE = 100;
 	private static final int PHOTO_REQUESTCODE = 200;
 	private static final String APPNAME = "hackathon";
